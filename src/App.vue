79baf37f6b9f7a2679ebfb713ca74fe8655d81d6
zhuanghaixin <649308293@qq.com>
2020-01-17 16:06:38 +0800

    cors跨域/JSONP跨域 安装axios jsonp插件

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      age: 30
+    };
+  },
+  mounted() {
+    let url =
+      "http://api.jirengu.com/readme2html.php?url=https://github.com/jirengu/server-mock";
+    axios.get(url).then(() => {});
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
