1851c03e9e09776bc73d0cef89662e43081df940
zhuanghaixin <649308293@qq.com>
2020-03-23 00:48:26 +0800

    fix(readme.md): 改变.env.prev的位置 使之生效，应在xiao...目录下

--- src/env.js
+++ src/env.js
@@ -1,21 +1,22 @@
-let baseURL
-switch (process.env.NODE_ENV) { //获取传过来的参数
+let baseURL;
+switch (process.env.NODE_ENV) {
     case 'development':
-        baseURL = 'http://dev-mall-pre.springboot.cn/api'
+        baseURL = 'http://dev-mall-pre.springboot.cn/api';
         break;
     case 'test':
-        baseURL = 'http://test-mall-pre.springboot.cn/api'
+        baseURL = 'http://test-mall-pre.springboot.cn/api';
         break;
     case 'prev':
-        baseURL = 'http://prev-mall-pre.springboot.cn/api'
+        baseURL = 'http://prev-mall-pre.springboot.cn/api';
         break;
-    case 'production':
-        baseURL = 'http://mall-pre.springboot.cn/api'
+    case 'prod':
+        baseURL = 'http://mall-pre.springboot.cn/api';
         break;
     default:
-        baseURL = 'http://mall-pre.springboot.cn/api'
+        baseURL = 'http://mall-pre.springboot.cn/api';
         break;
 }
+
 export default {
     baseURL
 }
