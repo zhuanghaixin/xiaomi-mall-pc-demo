9d34299525be9ace1633e1a55743808296722196
zhuanghaixin <649308293@qq.com>
2020-03-28 00:55:10 +0800

    fix(vue.config.js / App.vue): 理解了什么是接口代理

--- vue.config.js
+++ vue.config.js
@@ -5,12 +5,8 @@ module.exports = {
         proxy:{
             '/activity':{
                 target:'https://www.imooc.com',
-                changeOrigin:true,
+                changeOrigin:false,
                 pathRewrite:{
-                    //在App.vue中我想请求 /activity/servicetime ，然后因为proxy '/activity'拦截后，
-                    // 我其实请求的是 https://www.imooc.com/activity/servicetime
-                    //但是现在呢，我想访问https://www.imooc.com/course/list
-                    //所以我需要这样重写
                     '^/activity':'/course/list'
                 }
             }
