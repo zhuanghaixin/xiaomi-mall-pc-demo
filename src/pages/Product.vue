d75f27bf53f9e8072ec181b65caf4831d8df31b9
zhuanghaixin <649308293@qq.com>
2020-03-31 22:24:18 +0800

    feat(index.vue): 用swiper实现首页轮播功能

--- src/pages/Product.vue
+++ src/pages/Product.vue
@@ -1,10 +1,26 @@
  <template>
-  <div>product</div>
+  <div>product
+    <div>传的参数{{id}}</div>
+    <div></div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Product"
+  name: "Product",
+  data(){
+    return{
+      id:this.$route.params.id
+    }
+  },
+  watch:{
+    //当我们监听parms.id的变化，需要用到watch
+    // eslint-disable-next-line no-unused-vars
+    $route(to,from){
+      this.id=to.params.id
+    }
+  },
+
 };
 </script>
 
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,21 +1,124 @@
 <template>
-    <div class="service-bar-bottom">
+    <div class="index">
+        <div class="container">
+            <div class="swiper-box">
+                <swiper ref="mySwiper" :options="swiperOption">
+                    <swiper-slide v-for="(item,index) in slideList" :key="index">
+                        <a :href="'/#/product/'+item.id"><img :src="item.img"></a>
+                    </swiper-slide>
+                    <!-- Optional controls -->
+                    <div class="swiper-pagination" slot="pagination"></div>
+                    <div class="swiper-button-prev" slot="button-prev"></div>
+                    <div class="swiper-button-next" slot="button-next"></div>
+
+                </swiper>
+            </div>
+            <div class="ads-box"></div>
+            <div class="banner"></div>
+            <div class="product-box"></div>
+            <div></div>
+            <div>
+                <p><a href=""></a></p>
+                <p class="header"></p>
+            </div>
+        </div>
         <ServiceBar/>
     </div>
 </template>
 <script>
+    import 'swiper/css/swiper.css';
+    import {swiper, swiperSlide} from 'vue-awesome-swiper';
+
     import ServiceBar from "../components/TheServiceBar.vue";
 
     export default {
         name: "index",
         components: {
-            ServiceBar
+            ServiceBar,
+            swiper,
+            swiperSlide,
+
+        },
+        data() {
+            return {
+                swiperOption: {
+                    effect: 'fade',
+                    autoplay: true,
+                    loop: true,
+                    pagination: {
+                        el: '.swiper-pagination',
+                        clickable :true,
+                    },
+                    navigation: {
+                        nextEl: '.swiper-button-next',
+                        prevEl: '.swiper-button-prev',
+                    },
+                }, slideList: [
+                    {
+                        id: '42',
+                        img: '/imgs/slider/slide-1.jpg'
+                    }, {
+                        id: '45',
+                        img: '/imgs/slider/slide-2.jpg'
+                    }, {
+                        id: '46',
+                        img: '/imgs/slider/slide-3.jpg'
+                    }, {
+                        id: '47',
+                        img: '/imgs/slider/slide-4.jpg'
+                    }, {
+                        id: '48',
+                        img: '/imgs/slider/slide-5.jpg'
+                    }
+                ]
+            }
         }
     };
 </script>
 <style scoped lang="scss">
-  /*.service-bar-bottom{*/
-  /*  position: relative;*/
-  /*  bottom: 0px;*/
-  /*}*/
+    /*.service-bar-bottom{*/
+    /*  position: relative;*/
+    /*  bottom: 0px;*/
+    /*}*/
+    @import '../assets/scss/base.scss';
+    @import '../assets/scss/mixin.scss';
+
+    .index {
+        .container {
+            .swiper-box {
+                height: 451px;
+
+                img {
+                    width: 100%;
+                    height: 100%;
+                }
+
+                .swiper-button-prev {
+                    /*background-color:rgba(255,255,255,.1);*/
+                    color: rgba(51, 51, 51, .5);
+                    width: 47px;
+                    height: 84px;
+                }
+
+                .swiper-button-next {
+                    /*background-color:rgba(255,255,255,.1);*/
+                    color: rgba(51, 51, 51, .5);
+                    padding: 20px 10px;
+                }
+
+                .swiper-button-prev:hover {
+                    background-color: rgba(51, 51, 51, .5);
+                    color: #fff;
+                    cursor: pointer;
+                }
+
+                .swiper-button-next:hover {
+                    background-color: rgba(51, 51, 51, .5);
+                    color: #fff;
+                    cursor: pointer;
+                }
+
+            }
+        }
+    }
 </style>
