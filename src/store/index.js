717ce5bb5fa6b94c383cf13936f48ec120018e90
zhuanghaixin <649308293@qq.com>
2020-04-06 16:02:18 +0800

    fix(main.js): vuex环境搭建

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import * as actions from './actions';
+import * as getters from './getters';
+import mutations from "./mutations";
 Vue.use(Vuex)
 
+const state={
+  username:'', //登录用户名
+  cartCount:0  //购物车商品数量
+}
 export default new Vuex.Store({
-  state: {
+  state,
+  getters,
+  mutations,
+  actions
 
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
-  modules: {
-
-  }
 })
