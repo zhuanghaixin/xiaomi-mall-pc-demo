3058d594f32afac4e52ac2a99bf9230441977c79
zhuanghaixin <649308293@qq.com>
2020-04-11 23:44:40 +0800

    feat(OrderHeader.vue): 购物车列表渲染

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -2,14 +2,14 @@
     <div class="cart">
         <OrderHeader title="我的购物车">
             <template v-slot:tip>
-                    <span>温馨提示：产品是否购买成功，以最终下单为准哦，请尽快结算 j</span>
+                <span>温馨提示：产品是否购买成功，以最终下单为准哦，请尽快结算 j</span>
             </template>
         </OrderHeader>
         <div class="wrapper">
             <div class="container">
                 <div class="cart-box">
                     <ul class="cart-item-head">
-                        <li class="col-1"><span class="checkbox checked"></span>全选</li>
+                        <li class="col-1"><span class="checkbox checked" :class="{'checked':allChecked}"></span>全选</li>
                         <li class="col-3">商品名称</li>
                         <li class="col-1">单价</li>
                         <li class="col-2">数量</li>
@@ -17,23 +17,24 @@
                         <li class="col-1">操作</li>
                     </ul>
                     <ul class="cart-item-list">
-                        <li class="cart-item">
+                        <li class="cart-item" v-for="(item,index) in list" :key="index">
                             <div class="item-check">
-                                <span class="checkbox checked"></span>
+                                <span class="checkbox" :class="{'checked':item.productSelected}"></span>
                             </div>
                             <div class="item-name">
-                                <img src="/imgs/item-box-1.png">
-                                <span></span>
+                                <div class="item-img"><img v-lazy="item.productMainImage"></div>
+
+                                <span>{{item.productName}}</span>
                             </div>
-                            <div class="item-price">999</div>
+                            <div class="item-price">{{item.productPrice}}</div>
                             <div class="item-num">
                                 <div class="num-box">
-                                    <a href="javascript:;">-</a>
-                                    <span>1</span>
-                                    <a href="javascript:;" >+</a>
+                                    <a href="javascript:;" >-</a>
+                                    <span>{{item.quantity}}</span>
+                                    <a href="javascript:;">+</a>
                                 </div>
                             </div>
-                            <div class="item-total">999</div>
+                            <div class="item-total">{{item.productTotalPrice}}</div>
                             <div class="item-del"></div>
                         </li>
                     </ul>
@@ -41,15 +42,16 @@
                 <div class="order-wrap clearfix">
                     <div class="cart-tip fl">
                         <a href="/#/index">继续购物</a>
-                        共<span>2</span>件商品，已选择<span>2</span>件
+                        共<span>{{cartTotalNum}}</span>件商品，已选择<span>{{checkedNum}}</span>件
                     </div>
                     <div class="total fr">
-                        合计：<span>4896</span>元
+                        合计：<span>{{cartTotalPrice}}</span>元
                         <a href="javascript:;" class="btn">去结算</a>
                     </div>
                 </div>
             </div>
         </div>
+        <TheServiceBar/>
         <NavFooter/>
     </div>
 </template>
@@ -57,148 +59,214 @@
 <script>
     import NavFooter from "../components/TheNavFooter";
     import OrderHeader from '../components/OrderHeader.vue';
+    import TheServiceBar from '../components/TheServiceBar.vue';
+
     export default {
         name: "Cart",
         components: {
             NavFooter,
-            OrderHeader
+            OrderHeader,
+            TheServiceBar
+        },
+        mounted() {
+            this.getCartList()
+
+
+        },
+        data() {
+            return {
+                list: [],//商品列表
+                allChecked: false,//是否全选
+                cartTotalPrice: 0,//商品总金额
+                cartTotalNum: 0,//商品总数量
+                checkedNum:0 //选中的商品数量
+
+            }
+        },
+        methods: {
+            getCartList() {
+                this.axios.get('/carts').then((res) => {
+                    this.list = res.cartProductVoList || []  //没有返回的话，默认是空数组
+                    this.allChecked = res.selectedAll
+                    this.cartTotalPrice = res.cartTotalPrice
+                    this.cartTotalNum=res.cartTotalQuantity
+                    this.checkedNum=this.list.filter(item=>item.productSelected).reduce((acc,cur)=>{
+                        return acc+cur.quantity
+                    },0)
+
+                })
+            }
+
         }
+
     };
 </script>
 
 <style lang="scss" scoped>
     @import "../assets/scss/base.scss";
-    .cart{
-        .wrapper{
-            background-color:#F5F5F5;
-            padding-top:30px;
-            padding-bottom:114px;
-            .cart-box{
-                background-color:#fff;
-                font-size:14px;
-                color:#999999;
-                text-align:center;
-                .checkbox{
+
+    .cart {
+        .wrapper {
+            background-color: #F5F5F5;
+            padding-top: 30px;
+            padding-bottom: 114px;
+
+            .cart-box {
+                background-color: #fff;
+                font-size: 14px;
+                color: #999999;
+                text-align: center;
+
+                .checkbox {
                     display: inline-block;
                     width: 22px;
                     height: 22px;
                     border: 1px solid #E5E5E5;
                     vertical-align: middle;
                     margin-right: 17px;
-                    cursor:pointer;
-                    &.checked{
-                        background:url('/imgs/icon-gou.png') #FF6600 no-repeat center;
-                        background-size:16px 12px;
-                        border:none;
+                    cursor: pointer;
+
+                    &.checked {
+                        background: url('/imgs/icon-gou.png') #FF6600 no-repeat center;
+                        background-size: 16px 12px;
+                        border: none;
                     }
                 }
-                .cart-item-head{
-                    display:flex;
+
+                .cart-item-head {
+                    display: flex;
                     height: 79px;
                     line-height: 79px;
-                    .col-1{
-                        flex:1;
+
+                    .col-1 {
+                        flex: 1;
                     }
-                    .col-2{
-                        flex:2;
+
+                    .col-2 {
+                        flex: 2;
                     }
-                    .col-3{
-                        flex:3;
+
+                    .col-3 {
+                        flex: 3;
                     }
                 }
-                .cart-item-list{
-                    .cart-item{
-                        display:flex;
-                        align-items:center;
-                        height:125px;
-                        border-top:1px solid #E5E5E5;
-                        font-size:16px;
-                        .item-check{
-                            flex:1;
+
+                .cart-item-list {
+                    .cart-item {
+                        display: flex;
+                        align-items: center;
+                        height: 125px;
+                        border-top: 1px solid #E5E5E5;
+                        font-size: 16px;
+
+                        .item-check {
+                            flex: 1;
                         }
-                        .item-name{
-                            flex:3;
+
+                        .item-name {
+                            flex: 3;
                             font-size: 18px;
                             color: #333333;
                             display: flex;
                             align-items: center;
-                            img{
-                                width:80px;
-                                height:80px;
-                                vertical-align:middle;
-                            }
-                            span{
-                                margin-left: 30px;
-                            }
-                        }
-                        .item-price{
-                            flex:1;
-                            color:#333333;
-                        }
-                        .item-num{
-                            flex:2;
-                            .num-box{
-                                display:inline-block;
-                                height:40px;
-                                line-height:40px;
-                                border:1px solid #E5E5E5;
-                                font-size:14px;
-                                a{
-                                    display:inline-block;
-                                    width:50px;
-                                    color:#333333;
-                                }
-                                span{
-                                    display:inline-block;
-                                    width:50px;
-                                    color:#333333;
+                            .item-img{
+                                padding-right: 40px;
+
+                                img {
+                                    width: 80px;
+                                    height: 80px;
+                                    vertical-align: middle;
+
                                 }
                             }
+
+
+                            span {
+
+                            }
                         }
-                        .item-total{
-                            flex:1;
-                            color:#FF6600;
+
+                        .item-price {
+                            flex: 1;
+                            color: #333333;
                         }
-                        .item-del{
-                            flex:1;
-                            width:14px;
-                            height:12px;
-                            background:url('/imgs/icon-close.png') no-repeat center;
-                            background-size:contain;
-                            cursor:pointer;
+
+                        .item-num {
+                            flex: 2;
+
+                            .num-box {
+                                display: inline-block;
+                                height: 40px;
+                                line-height: 40px;
+                                border: 1px solid #E5E5E5;
+                                font-size: 14px;
+
+                                a {
+                                    display: inline-block;
+                                    width: 50px;
+                                    color: #333333;
+                                }
+
+                                span {
+                                    display: inline-block;
+                                    width: 50px;
+                                    color: #333333;
+                                }
+                            }
+                        }
+
+                        .item-total {
+                            flex: 1;
+                            color: #FF6600;
+                        }
+
+                        .item-del {
+                            flex: 1;
+                            width: 14px;
+                            height: 12px;
+                            background: url('/imgs/icon-close.png') no-repeat center;
+                            background-size: contain;
+                            cursor: pointer;
                         }
                     }
                 }
             }
-            .order-wrap{
-                font-size:14px;
+
+            .order-wrap {
+                font-size: 14px;
                 color: #666666;
                 margin-top: 20px;
                 height: 50px;
                 line-height: 50px;
-                .cart-tip{
+
+                .cart-tip {
                     margin-left: 29px;
-                    a{
+
+                    a {
                         color: #666666;
-                        margin-right:37px;
+                        margin-right: 37px;
                     }
-                    span{
-                        color:#FF6600;
-                        margin:0 5px;
+
+                    span {
+                        color: #FF6600;
+                        margin: 0 5px;
                     }
                 }
-                .total{
-                    font-size:14px;
-                    color:#FF6600;
-                    span{
-                        font-size:24px;
+
+                .total {
+                    font-size: 14px;
+                    color: #FF6600;
+
+                    span {
+                        font-size: 24px;
                     }
-                    a{
-                        width:202px;
-                        height:50px;
-                        line-height:50px;
-                        font-size:18px;
-                        margin-left:37px;
+
+                    a {
+                        width: 202px;
+                        height: 50px;
+                        line-height: 50px;
+                        font-size: 18px;
+                        margin-left: 37px;
                     }
                 }
             }
