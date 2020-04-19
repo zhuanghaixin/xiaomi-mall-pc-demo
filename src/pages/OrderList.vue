503e57ecb32048c507db4ffb38c70f01fbd9c6aa
zhuanghaixin <649308293@qq.com>
2020-04-19 22:51:35 +0800

    feat(OrderList.vue): 订单列表 滚动加载

--- src/pages/OrderList.vue
+++ src/pages/OrderList.vue
@@ -46,7 +46,7 @@
                             </div>
                         </div>
                     </div>
-                    <el-pagination
+                    <el-pagination v-if="false"
                             class="pagition"
                             background
                             layout="prev, pager, next"
@@ -55,10 +55,13 @@
                             @current-change="handleChange"
                     >
                     </el-pagination>
-                    <div class="load-more" v-if="false">
-                            <el-button type="primary" :loading="loading" @click="loadMore">加载更多</el-button>
+                    <div class="load-more" v-if="showNextPage">
+                        <el-button type="primary" :loading="loading" @click="buttonLoadMore">加载更多</el-button>
                     </div>
-
+                    <div class="scroll-more" v-infinite-scroll="loadMore" infinite-scroll-disabled="busy"
+                         infinite-scroll-distance="410" v-if="false">
+                        <img src="/imgs/loading-svg/loading-spinning-bubbles.svg" alt="" v-show="loading">
+                    </div>
                     <NoData v-if="!loading && list.length==0"></NoData>
                 </div>
             </div>
@@ -71,7 +74,8 @@
     import OrderHeader from '../components/OrderHeader.vue';
     import Loading from './../components/Loading.vue'
     import NoData from './../components/NoData.vue'
-    import {Button,Pagination} from 'element-ui'
+    import {Button, Pagination} from 'element-ui'
+    import infiniteScroll from 'vue-infinite-scroll'
 
     export default {
         name: "OrderList",
@@ -79,57 +83,114 @@
             OrderHeader,
             Loading,
             NoData,
-            [Pagination.name]:Pagination,
-            [Button.name]:Button
+            [Pagination.name]: Pagination,
+            [Button.name]: Button
         },
+        directives: {infiniteScroll},
         data() {
             return {
                 list: [],
-                loading:true,
-                pageSize:0,
-                total:0,
-                pageNum:1
+                loading: true,
+                pageSize: 0,
+                total: 0,
+                pageNum: 1,
+                busy: false, //滚动加载 ，是否触发
+                showNextPage:false //加载更多 是否
             }
         },
         mounted() {
-            this.goOrderList()
+            this.getOrderList()
+            // this.getOrderListMore()
         },
         methods: {
-
-            goOrderList(){
-                this.loading=true
-                this.axios.get('/orders',{
-                    params:{
-                        pageNum:this.pageNum,
-                        pageSize:4
+            getOrderList() {
+                this.loading = true
+                this.busy=true
+                this.axios.get('/orders', {
+                    params: {
+                        pageNum: this.pageNum,
+                        pageSize: 10
                     }
-                }).then((res)=>{
+                }).then((res) => {
+                    this.loading = false
+                    this.list = res.list  // 分页器
+                    this.pageSize = res.pageSize
+                    this.total = res.total
+                    this.busy=false
+                }).catch(()=>{
                     this.loading=false
-                    this.list= res.list  // 分页器
-                    this.pageSize=res.pageSize
-                    // this.list=this.list.concat(res.list) //加载更多2
-                    this.total=res.total
                 })
             },
-            goPay(orderNum){
+            //加载更多 获取订单列表
+            getOrderListMore() {
+                this.loading = true
+                // this.showNextPage=true
+                this.axios.get('/orders', {
+                    params: {
+                        pageNum: this.pageNum,
+                        pageSize: 10
+                    }
+                }).then((res) => {
+                    this.loading = false
+                    this.pageSize = res.pageSize
+                    this.list=this.list.concat(res.list) //加载更多
+                    this.total = res.total
+                   this.showNextPage=res.hasNextPage  //是否显示加载更多 按钮
+                    this.busy=false
+                }).catch(()=>{
+                    this.loading=false
+                })
+            },
+            goPay(orderNum) {
                 this.$router.push({
-                    name:'order-pay',
-                    query:{
+                    name: 'order-pay',
+                    query: {
                         orderNum
                     }
                 })
             },
             //分页器
-            handleChange(pageNum){
-                this.pageNum=pageNum
-                this.goOrderList()
+            handleChange(pageNum) {
+                this.pageNum = pageNum
+                this.getOrderList()
             },
             //加载更多
-            loadMore(){
+            buttonLoadMore() {
                 this.pageNum++
-                this.goOrderList()
-            }
+                this.getOrderListMore()
+            },
+            //滚动加载
+            loadMore: function () {
+                this.busy = true;
+                setTimeout(() => {
+                       this.pageNum++;
+                       this.getList()
 
+                    this.busy = false;
+                }, 1000)
+            },
+            //专门给scroll-more使用
+            getList() {
+                this.loading = true
+                this.axios.get('/orders', {
+                    params: {
+                        pageNum: this.pageNum,
+                        pageSize: 10
+                    }
+                }).then((res) => {
+                    this.loading = false
+                    this.list=this.list.concat(res.list) //加载更多
+                    if(res.list==[]){
+                        return
+                    }
+                    if(res.hasNextPage){
+                        this.busy=false
+                    }else{
+                        this.busy=true
+                    }
+
+                })
+            },
         }
     };
 </script>
@@ -137,74 +198,92 @@
 <style lang="scss">
     @import "../assets/scss/base.scss";
 
-    .order-list{
-        .wrapper{
-            background-color:$colorJ;
-            padding-top:33px;
-            padding-bottom:110px;
-            .order-box{
-                .order{
-                    border:1px solid $colorF;
-                    background-color:$colorG;
-                    margin-bottom:31px;
-                    &:last-child{
-                        margin-bottom:0;
+    .order-list {
+        .wrapper {
+            background-color: $colorJ;
+            padding-top: 33px;
+            padding-bottom: 110px;
+
+            .order-box {
+                .order {
+                    border: 1px solid $colorF;
+                    background-color: $colorG;
+                    margin-bottom: 31px;
+
+                    &:last-child {
+                        margin-bottom: 0;
                     }
-                    .order-title{
-                        @include height(74px,74px);
-                        background-color:$colorK;
-                        padding:0 43px;
-                        font-size:16px;
-                        color:$colorC;
-                        .item-info{
-                            span{
-                                margin:0 9px;
+
+                    .order-title {
+                        @include height(74px, 74px);
+                        background-color: $colorK;
+                        padding: 0 43px;
+                        font-size: 16px;
+                        color: $colorC;
+
+                        .item-info {
+                            span {
+                                margin: 0 9px;
                             }
                         }
-                        .money{
-                            font-size:26px;
-                            color:$colorB;
+
+                        .money {
+                            font-size: 26px;
+                            color: $colorB;
                         }
                     }
-                    .order-content{
-                        padding:0 43px;
-                        .good-box{
-                            .good-list{
+
+                    .order-content {
+                        padding: 0 43px;
+
+                        .good-box {
+                            .good-list {
                                 display: flex;
                                 align-items: center;
-                                height:145px;
-                                .good-img{
-                                    width:112px;
-                                    img{
-                                        width:100%;
+                                height: 145px;
+
+                                .good-img {
+                                    width: 112px;
+
+                                    img {
+                                        width: 100%;
                                     }
                                 }
-                                .good-name{
-                                    font-size:20px;
-                                    color:$colorB;
+
+                                .good-name {
+                                    font-size: 20px;
+                                    color: $colorB;
                                 }
                             }
                         }
-                        .good-state{
-                            @include height(145px,145px);
+
+                        .good-state {
+                            @include height(145px, 145px);
                             font-size: 20px;
-                            color:$colorA;
-                            a{
-                                color:$colorA;
+                            color: $colorA;
+
+                            a {
+                                color: $colorA;
                             }
                         }
                     }
                 }
-                .pagition{
-                    text-align:right
+
+                .pagition {
+                    text-align: right
                 }
-                .el-pagination.is-background .el-pager li:not(.disabled).active{
-                    background-color:$colorA;
-                    color:#FFF;
+
+                .el-pagination.is-background .el-pager li:not(.disabled).active {
+                    background-color: $colorA;
+                    color: #FFF;
                 }
-                .load-more{
-                text-align: center;
-            }
+
+                .load-more {
+                    text-align: center;
+                }
+                .scroll-more{
+                    text-align: center;
+                }
 
             }
         }
